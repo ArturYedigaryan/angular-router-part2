1dc043fb99885b20d17dd630359d50ce345a5c73
Artur Yedigaryan <artur.yedigaryan@acba.am>
2021-03-04 17:14:35 +0400

    first

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { CntactComponent } from './cntact/cntact.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    CntactComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule
